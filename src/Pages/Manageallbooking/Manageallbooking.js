00d17d65327ccad1722b33bcbaaf50e9ccc209c5
Tushar <freelancerrtushar@gmail.com>
2021-10-30 17:58:39 +0600

    changes

--- src/Pages/Manageallbooking/Manageallbooking.js
+++ src/Pages/Manageallbooking/Manageallbooking.js
@@ -1,6 +1,8 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios';
 import './Manageallbooking.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function Manageallbooking() {
 
     const [allbooking,setAllbooking]=useState([]);
@@ -18,11 +20,29 @@ function Manageallbooking() {
              axios.get(`https://quiet-sierra-01767.herokuapp.com/mystatuschange/${id}`)
                .then(res=>res).catch(err=>console.log(err))
              }
-         
-     
-    }
+     }
+
+     const handledeletebooking=(id)=>{
+        const confirmation=window.confirm("Are you sure you want to delete your booking");
+        if(confirmation===false){
+             return
+        }else{
+            axios.delete(`https://quiet-sierra-01767.herokuapp.com/deletebooking/${id}`)
+            .then(res=>{
+                toast("Booking deleted successfully");
+                if(res.deletedCount>0){
+                
+                let remainingbooking=allbooking.filter(booking=>booking._id!==id);
+                setAllbooking(remainingbooking)
+
+              }
+            console.log(res)
+            }).catch(err=>console.log(err));
+        }
+     }
     return (
         <div className="manage-wrapper">
+            <ToastContainer/>
              <h2 className="text-4xl font-bold text-center mb-5">Manage all Booking</h2>
              <div className="all-booking">
                 <table class="table-auto">
@@ -45,7 +65,7 @@ function Manageallbooking() {
                                       <td>{item.status}</td>
                                       <td>
                                         <button type="button" className="btn-primary" onClick={()=>handlestatus(item._id)}>status change</button>&nbsp;
-                                        <button  disabled  className="btn-danger">Delete</button>
+                                        <button className="btn-danger" onClick={()=>handledeletebooking(item._id)}>Delete</button>
                                       </td>
                                   </tr>
 
